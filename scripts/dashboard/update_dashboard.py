76110dd75f0742eb34a427c8e9a29fda9f12c3e1
Sandrikova Maria <mashasandrikova@gmail.com>
2018-03-02 18:06:31 +0000

    Add plots and an architecture scheme generation

--- scripts/dashboard/update_dashboard.py
+++ scripts/dashboard/update_dashboard.py
@@ -4,6 +4,11 @@ Description: Update information on dashboard webpage
 import os
 import yaml
 import markdown2
+import demjson
+import plotly.offline as of_py
+import plotly.graph_objs as go
+from keras.models import model_from_json
+from keras.utils.vis_utils import plot_model
 
 WORKING_DIR = os.path.dirname(__file__)
 ROOT_DIR = os.path.join(WORKING_DIR, '..', '..')
@@ -13,14 +18,66 @@ DASHBOARD_CONFIG = os.path.join(WORKING_DIR, 'dashboard_config.yaml')
 DESCRIPTION_TEMPLATE = os.path.join(WORKING_DIR, 'description_template.txt')
 
 
+def generate_model_scheme(experiment_dir, dashboard_config):
+    """
+    Save a model's architecture as image 
+    """
+    model_path = os.path.join(
+        experiment_dir,
+        dashboard_config['model_file']
+    )
+    scheme_path = os.path.join(
+        experiment_dir,
+        dashboard_config['plots_dir'],
+        dashboard_config['scheme_name']
+    )
+    with open(model_path, 'r') as json_file:
+        model = model_from_json(json_file.read())
+    plot_model(model, to_file=scheme_path, show_shapes=True)
+
+def produce_graph_data(key, graph):
+    """
+    Extract the data for a graph
+    """
+    return (list(range(0, len(graph[key]))), graph[key])
+
+def plot_metrics(experiment_dir, dashboard_config, plot_name, first_metric, second_metric):
+    """
+    Plot two given metrics on one graph
+    """
+    log_path = os.path.join(experiment_dir, dashboard_config['log_file'])
+    with open(log_path) as log_file:
+        graph = demjson.decode(log_file.read())
+    x_values, y_values = produce_graph_data(first_metric, graph)
+    trace1 = go.Scatter(
+        x=x_values,
+        y=y_values,
+        marker=dict(color='#000099'),
+        name=first_metric)
+    x_values, y_values = produce_graph_data(second_metric, graph)
+    trace2 = go.Scatter(
+        x=x_values,
+        y=y_values,
+        marker=dict(color='#ffa500'),
+        name=second_metric)
+    of_py.plot(
+        [trace1, trace2],
+        filename=os.path.join(experiment_dir, dashboard_config['plots_dir'], plot_name),
+        auto_open=False
+    )
+
 def generate_description_markdown(dashboard_config, experiment_config, experiment_dir):
     """ Generate an experiment's description """
     with open(DESCRIPTION_TEMPLATE) as md_file:
         data = md_file.read()
     data = data.format(
         experiment_config['experiment_name'],
+        experiment_config['description'],
+        experiment_config['data_dir'],
+        experiment_config['loss'],
+        experiment_config['optimizer'],
         experiment_config['num_epoches'],
-        experiment_config['description']
+        experiment_config['batch_size']
     )
     html_data = markdown2.markdown(data)
     result_file = os.path.join(
@@ -38,14 +95,17 @@ def generate_plots_for_experiment(dashboard_config, experiment_dir):
         experiment_config = yaml.load(yaml_file)
     if not os.path.exists(os.path.join(experiment_dir, dashboard_config['plots_dir'])):
         os.makedirs(os.path.join(experiment_dir, dashboard_config['plots_dir']))
-    generate_description_markdown(dashboard_config, experiment_config, experiment_dir)
-
+    else:
+        #return
+        pass
+    plot_metrics(experiment_dir, dashboard_config, dashboard_config['loss_plot_name'], 'loss', 'val_loss')
+    plot_metrics(experiment_dir, dashboard_config, dashboard_config['acc_plot_name'], 'acc', 'val_acc')
+    generate_model_scheme(experiment_dir, dashboard_config)
 
 def generate_experiments_dir_js(dashboard_config):
     """ Generate file experiments_dir.js """
     with open(os.path.join(DASHBOARD_ROOT, 'static', 'js', 'experiments_dir.js'), 'w+') as js_file:
         js_file.write("experiments_dir = {};".format(dashboard_config['experiments_dir']))
-
 
 def execute():
     """ Execute script """
