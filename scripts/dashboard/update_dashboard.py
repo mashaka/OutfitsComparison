e4ad06192d1e194f3698068ccbb802d34ce023c3
Sandrikova Maria <mashasandrikova@gmail.com>
2018-03-03 15:31:22 +0000

    Return unintentionally deleted description generation

--- scripts/dashboard/update_dashboard.py
+++ scripts/dashboard/update_dashboard.py
@@ -98,6 +98,7 @@ def generate_plots_for_experiment(dashboard_config, experiment_dir):
     else:
         #return
         pass
+    generate_description_markdown(dashboard_config, experiment_config, experiment_dir)
     plot_metrics(experiment_dir, dashboard_config, dashboard_config['loss_plot_name'], 'loss', 'val_loss')
     plot_metrics(experiment_dir, dashboard_config, dashboard_config['acc_plot_name'], 'acc', 'val_acc')
     generate_model_scheme(experiment_dir, dashboard_config)
