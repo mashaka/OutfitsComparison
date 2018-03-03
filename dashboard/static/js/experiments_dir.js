e36b7e9e6b034cd03f0760aceeed5a851fc75bab
Sandrikova Maria <mashasandrikova@gmail.com>
2018-03-03 15:32:34 +0000

    Add results of a fully trained autumn NIR network

--- dashboard/static/js/experiments_dir.js
+++ dashboard/static/js/experiments_dir.js
@@ -1,1 +1,1 @@
-experiments_dir = ['baseline/results/Xception_3', 'baseline/results/autumn_nir'];
+experiments_dir = ['baseline/results/Xception_3', 'baseline/results/autumn_nir', 'baseline/results/Autumn_NIR_retry'];
